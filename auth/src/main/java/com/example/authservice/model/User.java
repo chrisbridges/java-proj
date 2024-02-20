67e74ceadb47edef463116b73b5a3cfe98097ac1
Chris Bridges <christopher_bridges@icloud.com>
2024-02-20 09:05:46 -0600

    user entity methods

--- auth/src/main/java/com/example/authservice/model/User.java
+++ auth/src/main/java/com/example/authservice/model/User.java
@@ -10,10 +10,50 @@ public class User {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(unique = true)
+  @Column(nullable = false, unique = true)
   private String username;
 
+  @Column(nullable = false)
   private String password;
 
-  // Constructors, getters, and setters
+  // Default constructor for JPA
+  public User() {
+  }
+
+  // All-args constructor for convenience
+  public User(Long id, String username, String password) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+  }
+
+  // Getters and setters
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  // Convenience method for updating password
+  public void updatePassword(String newPassword) {
+    this.password = newPassword;
+  }
 }
