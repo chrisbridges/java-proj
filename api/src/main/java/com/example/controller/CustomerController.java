243248e8ad1d1f8b74b2721273c78a37e64355a2
Chris Bridges <christopher_bridges@icloud.com>
2024-02-20 12:06:23 -0600

    its alive!

--- api/src/main/java/com/example/controller/CustomerController.java
+++ api/src/main/java/com/example/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import com.example.model.Customer;
 import com.example.customerService.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class CustomerController {
   @Autowired
   private CustomerService customerService;
 
+  @CrossOrigin(origins = "http://localhost:3000")
   @GetMapping
   public List<Customer> getAllCustomers() {
     return customerService.getAllCustomers();
--- auth/src/main/java/com/example/authservice/config/SecurityConfig.java
+++ auth/src/main/java/com/example/authservice/config/SecurityConfig.java
@@ -13,7 +13,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable()
+    http.cors().and()
+        .csrf().disable()
         .authorizeRequests()
         .antMatchers("/auth/**").permitAll()
         // .cors().and() // disable CORS
